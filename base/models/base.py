e03b32cb5d37c4b531e8c3baed86f2d92709630d
Chmele <kkk@kkk.kkk>
2022-01-13 03:33:22 +0200

    provide base models for PointList, Table

---
+++ base/models/base.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from typing import Any
+
+
+class Point(BaseModel):
+    x: float = 0
+    y: float = 0
+
+
+class PointList(BaseModel):
+    points: list[Point] = []
+
+
+class TableCell(BaseModel):
+    content: Any
+
+
+class TableRow(BaseModel):
+    cells: list[TableCell] = []
+
+
+class Table(BaseModel):
+    rows: list[TableRow] = []
+    
+
+class HeaderTable(Table):
+    headers = list[str]
