fce9a6960eae6a2cd42e816cdc50be19260d1fce
Artyom Fisunenko <artyom.fisunenko@gmail.com>
2022-05-10 14:25:31 +0300

    fix test according to graham method fix

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -18,16 +18,16 @@ class TestTasks(TestCase):
             Point(3,2),
             Point(2,4)
         ]))
-        internal_point = GrahamPoint(x=4.666666666666667, y=2.0)
+        centroid = GrahamPoint(x=4.666666666666667, y=2.0)
         ordered = GrahamPointList(points=[
-            GrahamPoint(x=1, y=0),
             GrahamPoint(x=4, y=0),
             GrahamPoint(x=6, y=4),
             GrahamPoint(x=2, y=4),
             GrahamPoint(x=4, y=2),
-            GrahamPoint(x=3, y=2)
+            GrahamPoint(x=3, y=2),
+            GrahamPoint(x=1, y=0)
         ])
-        origin = GrahamPoint(x=1, y=0)
+        origin = GrahamPoint(x=4, y=0)
         steps = GrahamTable(rows=[
             GrahamTableRow(cells=(
                 GrahamTrinityCell(content=(ordered.points[0], ordered.points[1], ordered.points[2])),
@@ -43,37 +43,37 @@ class TestTasks(TestCase):
             )),
             GrahamTableRow(cells=(
                 GrahamTrinityCell(content=(ordered.points[2], ordered.points[3], ordered.points[4])),
-                GrahamPiCompareCell(content=PiCompare.less),
+                GrahamPiCompareCell(content=PiCompare.more),
                 GrahamCenterPointCell(content=ordered.points[3]),
+                GrahamToAddCell(content=ToAddGraham.no)
+            )),
+            GrahamTableRow(cells=(
+                GrahamTrinityCell(content=(ordered.points[1], ordered.points[2], ordered.points[4])),
+                GrahamPiCompareCell(content=PiCompare.less),
+                GrahamCenterPointCell(content=ordered.points[2]),
                 GrahamToAddCell(content=ToAddGraham.yes)
             )),
             GrahamTableRow(cells=(
-                GrahamTrinityCell(content=(ordered.points[3], ordered.points[4], ordered.points[5])),
+                GrahamTrinityCell(content=(ordered.points[2], ordered.points[4], ordered.points[5])),
                 GrahamPiCompareCell(content=PiCompare.more),
                 GrahamCenterPointCell(content=ordered.points[4]),
                 GrahamToAddCell(content=ToAddGraham.no)
             )),
             GrahamTableRow(cells=(
-                GrahamTrinityCell(content=(ordered.points[2], ordered.points[3], ordered.points[5])),
+                GrahamTrinityCell(content=(ordered.points[1], ordered.points[2], ordered.points[5])),
                 GrahamPiCompareCell(content=PiCompare.less),
-                GrahamCenterPointCell(content=ordered.points[3]),
+                GrahamCenterPointCell(content=ordered.points[2]),
                 GrahamToAddCell(content=ToAddGraham.yes)
             )),
             GrahamTableRow(cells=(
-                GrahamTrinityCell(content=(ordered.points[3], ordered.points[5], ordered.points[0])),
-                GrahamPiCompareCell(content=PiCompare.more),
-                GrahamCenterPointCell(content=ordered.points[5]),
-                GrahamToAddCell(content=ToAddGraham.no)
-            )),
-            GrahamTableRow(cells=(
-                GrahamTrinityCell(content=(ordered.points[2], ordered.points[3], ordered.points[0])),
+                GrahamTrinityCell(content=(ordered.points[2], ordered.points[5], ordered.points[0])),
                 GrahamPiCompareCell(content=PiCompare.less),
-                GrahamCenterPointCell(content=ordered.points[3]),
+                GrahamCenterPointCell(content=ordered.points[5]),
                 GrahamToAddCell(content=ToAddGraham.yes)
             )),
         ])
         
-        self.assertAlmostEqual(task.stages[0].items[0].answer, internal_point)
+        self.assertAlmostEqual(task.stages[0].items[0].answer, centroid)
         self.assertEqual(task.stages[1].items[0].answer, ordered)
         self.assertEqual(task.stages[2].items[0].answer, origin)
         self.assertEqual(task.stages[3].items[0].answer, steps)
