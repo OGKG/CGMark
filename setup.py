d433b184ce165c7059882ceab19427e8f4191024
Artyom Fisunenko <artyom.fisunenko@gmail.com>
2023-06-22 23:00:03 +0300

    fix authors in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='MarkLib',
-      version='1.1.3',
+      version='1.1.4',
       description='',
       url='https://github.com/OGKG/PyMarkLib',
-      author='Vsevolod Hermaniuk',
-      author_email='shermanyuk@knu.ua',
+      author='Vsevolod Hermaniuk & artandfi (Artem Fisunenko)',
       license='MIT',
     #   packages=['MarkLib'],
       install_requires=[
